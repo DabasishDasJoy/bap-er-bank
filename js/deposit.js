0caba14e4a2bfe543e139bcb9c206fe0a916ba4a
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-08-17 21:29:21 +0600

    make it functional

--- js/deposit.js
+++ js/deposit.js
@@ -1,31 +1,45 @@
-document.getElementById('btn-deposit').addEventListener('click', function () {
-    console.log('clicked');
-    const newDepositAmount = document.getElementById('deposit-field');
-    const depositAmountElement = document.getElementById('deposit-amount');
-    const balanceAmoutElement = document.getElementById('ac-balance');
-
-    const currentDepositAmount = depositAmountElement.innerText;
-    const currentBalanceAmount = balanceAmoutElement.innerText;
-
-    const newDepositAmountValue = parseFloat(newDepositAmount.value);
-    const currentBalanceAmountValue = parseFloat(currentBalanceAmount);
-    const currentDepositAmountValue = parseFloat(currentDepositAmount);
-
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    const newDepositAmount = getInputFieldValueByID('deposit-field');
     
-    if(!isNaN(newDepositAmountValue)){
-        depositAmountElement.innerText = currentDepositAmountValue + newDepositAmountValue;
-        balanceAmoutElement.innerText = currentBalanceAmountValue + newDepositAmountValue;
-        
+    if(!isNaN(newDepositAmount)){
+        const prevDepositAmount = getElementValueById('total-deposit-amount');
+        const prevTotalBalance = getElementValueById('total-ac-balance');
+        setElementValueById('total-deposit-amount', newDepositAmount + prevDepositAmount);
+        setElementValueById('total-ac-balance', prevTotalBalance + newDepositAmount);
     }
     else{
-        // document.getElementById('btn-deposit').setAttribute('data-bs-toggle', 'modal');
-        // document.getElementById('btn-deposit').setAttribute('data-bs-target', "#exampleModalCenter");
-        
-        alert('Invalid');
+        alert("Invalid operation");
     }
     
-    newDepositAmount.value = "";
 });
+
+// document.getElementById('btn-deposit').addEventListener('click', function () {
+//     console.log('clicked');
+//     const newDepositAmount = document.getElementById('deposit-field');
+//     const depositAmountElement = document.getElementById('deposit-amount');
+//     const balanceAmoutElement = document.getElementById('ac-balance');
+
+//     const currentDepositAmount = depositAmountElement.innerText;
+//     const currentBalanceAmount = balanceAmoutElement.innerText;
+
+//     const newDepositAmountValue = parseFloat(newDepositAmount.value);
+//     const currentBalanceAmountValue = parseFloat(currentBalanceAmount);
+//     const currentDepositAmountValue = parseFloat(currentDepositAmount);
+
+    
+//     if(!isNaN(newDepositAmountValue)){
+//         depositAmountElement.innerText = currentDepositAmountValue + newDepositAmountValue;
+//         balanceAmoutElement.innerText = currentBalanceAmountValue + newDepositAmountValue;
+        
+//     }
+//     else{
+//         // document.getElementById('btn-deposit').setAttribute('data-bs-toggle', 'modal');
+//         // document.getElementById('btn-deposit').setAttribute('data-bs-target', "#exampleModalCenter");
+//         alert('Invalid');
+//     }
+    
+//     newDepositAmount.value = "";
+// });
 
 
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,29 +1,45 @@
-document.getElementById('btn-withdraw').addEventListener('click', function () {
-    const newWithdrawAmount = document.getElementById('withdraw-field');
-    const withdrawAmountElement = document.getElementById('withdraw-amount');
-    const balanceAmoutElement = document.getElementById('ac-balance');
-
-    const currentWithdrawAmount = withdrawAmountElement.innerText;
-    const currentBalanceAmount = balanceAmoutElement.innerText;
-
-    const newWithdrawAmountValue = parseFloat(newWithdrawAmount.value);
-    const currentBalanceAmountValue = parseFloat(currentBalanceAmount);
-    const currentWithdrawAmountValue = parseFloat(currentWithdrawAmount);
-
-    if(newWithdrawAmountValue <= currentBalanceAmountValue){
-        withdrawAmountElement.innerText = currentWithdrawAmountValue + newWithdrawAmountValue;
-        balanceAmoutElement.innerText = currentBalanceAmountValue - newWithdrawAmountValue;
-    }
-    else if(isNaN(newWithdrawAmountValue)){
-        
-        alert('Invalid input');
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const newWithdrawAmount = getInputFieldValueByID('withdraw-field');
+    const prevTotalBalance = getElementValueById('total-ac-balance');
+    
+    if(!isNaN(newWithdrawAmount) && newWithdrawAmount <= prevTotalBalance){
+        const prevWithdrawAmount = getElementValueById('total-withdraw-amount');
+        setElementValueById('total-withdraw-amount', newWithdrawAmount + prevWithdrawAmount);
+        setElementValueById('total-ac-balance', prevTotalBalance - newWithdrawAmount);
     }
     else{
-        alert("Insufficient Amount");
+        alert('Invalid');
     }
+})
+
+
+
+// document.getElementById('btn-withdraw').addEventListener('click', function () {
+//     const newWithdrawAmount = document.getElementById('withdraw-field');
+//     const withdrawAmountElement = document.getElementById('withdraw-amount');
+//     const balanceAmoutElement = document.getElementById('ac-balance');
+
+//     const currentWithdrawAmount = withdrawAmountElement.innerText;
+//     const currentBalanceAmount = balanceAmoutElement.innerText;
+
+//     const newWithdrawAmountValue = parseFloat(newWithdrawAmount.value);
+//     const currentBalanceAmountValue = parseFloat(currentBalanceAmount);
+//     const currentWithdrawAmountValue = parseFloat(currentWithdrawAmount);
+
+//     if(newWithdrawAmountValue <= currentBalanceAmountValue){
+//         withdrawAmountElement.innerText = currentWithdrawAmountValue + newWithdrawAmountValue;
+//         balanceAmoutElement.innerText = currentBalanceAmountValue - newWithdrawAmountValue;
+//     }
+//     else if(isNaN(newWithdrawAmountValue)){
+        
+//         alert('Invalid input');
+//     }
+//     else{
+//         alert("Insufficient Amount");
+//     }
     
-    newWithdrawAmount.value = "";
-});
+//     newWithdrawAmount.value = "";
+// });
 
 
 
